38658f2fb76b34686b221b360eff4770552348cf
Joe Guest <joe.a.guest@gmail.com>
2015-10-11 22:31:46 +0100

    Partials created, cuke updated
    HTML tags added prior to styling, just to break the page up

--- display_bill.rb
+++ display_bill.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 require './app/models/api/bill'
+require './app/decorators/bill_decorator'
+require './app/decorators/bill_decorator/call_charges_decorator'
+require './app/decorators/bill_decorator/package_decorator'
+require './app/decorators/bill_decorator/sky_store_decorator'
+require './app/decorators/bill_decorator/statement_decorator'
 
 set :views, Proc.new { File.join(root, "app/views") }
 
 get '/' do
-  erb :index, locals: { bill: API::Bill.get }
+  erb :index, locals: { bill: decorated_bill }
+end
+
+def decorated_bill
+  BillDecorator.decorate(bill)
+end
+
+def bill
+  API::Bill.get
 end
--- features/step_definitions/bill_details.rb
+++ features/step_definitions/bill_details.rb
@@ -3,21 +3,21 @@ Given(/^user is viewing their Sky bill$/) do
 end
 
 Then(/^they can see their statement$/) do
-   expect(page).to have_content("statement")
+   expect(page).to have_content("Statement")
 end
 
 And(/^their bills total$/) do
-   expect(page).to have_content("total")
+   expect(page).to have_content("Total")
 end
 
 And(/^their subscription package$/) do
-   expect(page).to have_content("package")
+   expect(page).to have_content("Package")
 end
 
 And(/^their call charges$/) do
-   expect(page).to have_content("callCharges")
+   expect(page).to have_content("Call Charges")
 end
 
 And(/^their SkyStore details$/) do
-   expect(page).to have_content("skyStore")
+   expect(page).to have_content("Sky Store")
 end
