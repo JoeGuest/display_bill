08fdae55b1488e541db64a040b9033fafdb52b67
Joe Guest <joe.a.guest@gmail.com>
2015-10-12 08:16:57 +0100

    CallChargesDecorator updated, with spec

--- app/decorators/bill_decorator/call_charges_decorator.rb
+++ app/decorators/bill_decorator/call_charges_decorator.rb
@@ -1,7 +1,51 @@
 class CallChargesDecorator
+  def initialize(call_charges)
+    @call_charges = call_charges
+  end
+
+  def self.decorate(call_charges)
+    new(call_charges)
+  end
+
   def calls
+    call_list.map do |call|
+      CallDecorator.decorate(call)
+    end
   end
 
   def total
+    call_charges['total']
+  end
+
+  private
+  attr_reader :call_charges
+
+  def call_list
+    call_charges['calls']
+  end
+
+  class CallDecorator
+    def initialize(call)
+      @call = call
+    end
+
+    def self.decorate(call)
+      new(call)
+    end
+
+    def called
+      call['called']
+    end
+
+    def duration
+      call['duration']
+    end
+
+    def cost
+      call['cost']
+    end
+
+    private
+    attr_reader :call
   end
 end
--- spec/decorators/bill_decorator/call_charges_decorator_spec.rb
+++ spec/decorators/bill_decorator/call_charges_decorator_spec.rb
@@ -1,29 +1,44 @@
 require './app/decorators/bill_decorator/call_charges_decorator'
 
 describe CallChargesDecorator do
+  let(:decorator) { CallChargesDecorator.decorate(call_charges) }
+  let(:call_charges) {
+    {
+      'calls' => [call],
+      'total' => '100.00'
+    }
+  }
+  let(:call) {
+    {
+      'called' => '0123456789',
+      'duration' => '00:11:22',
+      'cost' => '1.23'
+    }
+  }
+
   describe '#calls' do
-    it 'returns all the calls made' do
-      pending
-    end
+    let(:decorated_calls) { decorator.calls }
 
     context 'each call' do
+      let(:decorated_call) { decorated_calls.first }
+
       it 'returns the connected number' do
-        pending
+        expect(decorated_call.called).to eql('0123456789')
       end
 
       it 'returns the duration' do
-        pending
+        expect(decorated_call.duration).to eql('00:11:22')
       end
 
       it 'returns the cost' do
-        pending
+        expect(decorated_call.cost).to eql('1.23')
       end
     end
   end
 
   describe '#total' do
     it 'returns the total of all the calls' do
-      pending
+      expect(decorator.total).to eql('100.00')
     end
   end
 end
