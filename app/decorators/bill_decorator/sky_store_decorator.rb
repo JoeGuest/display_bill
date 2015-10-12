882492dce1734ba12a54229644dc887d849fcc9f
Joe Guest <joe.a.guest@gmail.com>
2015-10-12 08:17:35 +0100

    SkyStoreDecorator updated, with spec

--- app/decorators/bill_decorator/sky_store_decorator.rb
+++ app/decorators/bill_decorator/sky_store_decorator.rb
@@ -1,10 +1,57 @@
 class SkyStoreDecorator
+  def initialize(sky_store)
+    @sky_store = sky_store
+  end
+
+  def self.decorate(sky_store)
+    new(sky_store)
+  end
+
   def rentals
+    rentals_list.map do |rental|
+      MediaItem.decorate(rental)
+    end
   end
 
   def buy_and_keep
+    buy_and_keep_list.map do |buy_and_keep|
+      MediaItem.decorate(buy_and_keep)
+    end
   end
 
   def total
+    sky_store['total']
+  end
+
+  private
+  attr_reader :sky_store
+
+  def rentals_list
+    sky_store['rentals']
+  end
+
+  def buy_and_keep_list
+    sky_store['buyAndKeep']
+  end
+
+  class MediaItem
+    def initialize(item)
+      @item = item
+    end
+
+    def self.decorate(item)
+      new(item)
+    end
+
+    def title
+      item['title']
+    end
+
+    def cost
+      item['cost']
+    end
+
+    private
+    attr_reader :item
   end
 end
--- spec/decorators/bill_decorator/sky_store_decorator_spec.rb
+++ spec/decorators/bill_decorator/sky_store_decorator_spec.rb
@@ -1,41 +1,62 @@
 require './app/decorators/bill_decorator/sky_store_decorator'
 
 describe SkyStoreDecorator do
+  let(:decorator) { SkyStoreDecorator.decorate(sky_store) }
+  let(:sky_store) {
+    {
+      'rentals' => [rental],
+      'buyAndKeep' => [buy_and_keep],
+      'total' => '100.00'
+    }
+  }
+  let(:rental) {
+    {
+      'title' => 'film',
+      'cost' => '1.23'
+    }
+  }
+  let(:buy_and_keep) {
+    {
+      'title' => 'tv',
+      'cost' => '9.87'
+    }
+  }
+
   describe '#rentals' do
-    it 'returns all rentals' do
-      pending
-    end
+    let(:decorated_rentals) { decorator.rentals }
 
     context 'each rental' do
+      let(:decorated_rental) { decorated_rentals.first }
+
       it 'returns the title' do
-        pending
+        expect(decorated_rental.title).to eql('film')
       end
 
       it 'returns the cost' do
-        pending
+        expect(decorated_rental.cost).to eql('1.23')
       end
     end
   end
 
   describe '#buy_and_keep' do
-    it 'returns all bought media' do
-      pending
-    end
+    let(:decorated_purchases) { decorator.buy_and_keep }
 
     context 'each purchase' do
+      let(:decorated_purchase) { decorated_purchases.first }
+
       it 'returns the title' do
-        pending
+        expect(decorated_purchase.title).to eql('tv')
       end
 
       it 'returns the cost' do
-        pending
+        expect(decorated_purchase.cost).to eql('9.87')
       end
     end
   end
 
   describe '#total' do
     it 'returns the total of all Sky Store purchases' do
-      pending
+      expect(decorator.total).to eql('100.00')
     end
   end
 end
