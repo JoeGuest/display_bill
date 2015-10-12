09fa81f2d36f205ce9f32c12a54cc3ca5cdb57a0
Joe Guest <joe.a.guest@gmail.com>
2015-10-12 08:16:28 +0100

    BillDecorator updated, with spec

--- app/decorators/bill_decorator.rb
+++ app/decorators/bill_decorator.rb
@@ -1,16 +1,49 @@
 class BillDecorator
+  def initialize(bill)
+    @bill = bill
+  end
+
+  def self.decorate(bill)
+    new(bill)
+  end
+
   def statement
+    StatementDecorator.decorate(statement_details)
   end
 
   def total
+    bill['total']
   end
 
   def package
+    PackageDecorator.decorate(package_details)
   end
 
   def call_charges
+    CallChargesDecorator.decorate(call_details)
   end
 
   def sky_store
+    SkyStoreDecorator.decorate(sky_store_details)
+  end
+
+  private
+
+  attr_reader :bill
+
+  def statement_details
+    bill['statement']
+  end
+
+  def package_details
+    bill['package']
+  end
+
+  def call_details
+    bill['callCharges']
+  end
+
+  def sky_store_details
+    bill['skyStore']
   end
 end
--- spec/decorators/bill_decorator_spec.rb
+++ spec/decorators/bill_decorator_spec.rb
@@ -1,27 +1,80 @@
 require './app/decorators/bill_decorator'
+require './app/decorators/bill_decorator/statement_decorator'
+require './app/decorators/bill_decorator/package_decorator'
+require './app/decorators/bill_decorator/call_charges_decorator'
+require './app/decorators/bill_decorator/sky_store_decorator'
 
 describe BillDecorator do
+  let(:decorator) { BillDecorator.decorate(bill) }
+  let(:bill) {
+    {
+      'statement' => 'statement',
+      'total' => '100.00',
+      'package' => 'subscriptions',
+      'callCharges' => 'calls',
+      'skyStore' => 'purchases'
+    }
+  }
+
   describe '#statement' do
+    let(:decorated_statement) { double('StatementDecorator') }
+
+    before do
+      allow(StatementDecorator).to receive(:decorate).
+        with(bill['statement']).
+        and_return(decorated_statement)
+    end
+
     it 'decorates the statement' do
-      pending
+      expect(decorator.statement).to eql(decorated_statement)
+    end
+  end
+
+  describe '#total' do
+    it 'returns the total bill price' do
+      expect(decorator.total).to eql('100.00')
     end
   end
 
   describe '#package' do
+    let(:decorated_package) { double('PackageDecorator') }
+
+    before do
+      allow(PackageDecorator).to receive(:decorate).
+        with(bill['package']).
+        and_return(decorated_package)
+    end
+
     it 'decorates the package' do
-      pending
+      expect(decorator.package).to eql(decorated_package)
     end
   end
 
   describe '#call_charges' do
+    let(:decorated_call_charges) { double('CallChargesDecorator') }
+
+    before do
+      allow(CallChargesDecorator).to receive(:decorate).
+        with(bill['callCharges']).
+        and_return(decorated_call_charges)
+    end
+
     it 'decorates the call charges' do
-      pending
+      expect(decorator.call_charges).to eql(decorated_call_charges)
     end
   end
 
   describe '#sky_store' do
+    let(:decorated_sky_store) { double('SkyStoreDecorator') }
+
+    before do
+      allow(SkyStoreDecorator).to receive(:decorate).
+        with(bill['skyStore']).
+        and_return(decorated_sky_store)
+    end
+
     it 'decorates the sky store' do
-      pending
+      expect(decorator.sky_store).to eql(decorated_sky_store)
     end
   end
 end
