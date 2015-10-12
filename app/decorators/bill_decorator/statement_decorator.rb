6695de62a8d92a9f190ad54e6dc3e6665c290a98
Joe Guest <joe.a.guest@gmail.com>
2015-10-12 08:17:46 +0100

    StatementDecorator updated, with spec

--- app/decorators/bill_decorator/statement_decorator.rb
+++ app/decorators/bill_decorator/statement_decorator.rb
@@ -1,10 +1,49 @@
 class StatementDecorator
+  def initialize(statement)
+    @statement = statement
+  end
+
+  def self.decorate(statement)
+    new(statement)
+  end
+
   def generated
+    statement['generated']
   end
 
   def due
+    statement['due']
   end
 
   def period
+    BillPeriod.decorate(bill_period_details)
+  end
+
+  private
+  attr_reader :statement
+
+  def bill_period_details
+    statement['period']
+  end
+
+  class BillPeriod
+    def initialize(bill_period)
+      @bill_period = bill_period
+    end
+
+    def self.decorate(bill_period)
+      new(bill_period)
+    end
+
+    def from
+      bill_period['from']
+    end
+
+    def to
+      bill_period['to']
+    end
+
+    private
+    attr_reader :bill_period
   end
 end
--- spec/decorators/bill_decorator/statement_decorator_spec.rb
+++ spec/decorators/bill_decorator/statement_decorator_spec.rb
@@ -1,21 +1,38 @@
 require './app/decorators/bill_decorator/statement_decorator'
 
 describe StatementDecorator do
+  let(:decorator) { StatementDecorator.decorate(statement) }
+  let(:statement) {
+    {
+      'generated' => '2015-10-11',
+      'due' => '2015-11-11',
+      'period' => period
+    }
+  }
+  let(:period) {
+    {
+      'from' => '2015-09-05',
+      'to' => '2015-10-05'
+    }
+  }
+
   it 'returns when the bill was generated' do
-    pending
+    expect(decorator.generated).to eql('2015-10-11')
   end
 
   it 'returns when the bill is due' do
-    pending
+    expect(decorator.due).to eql('2015-11-11')
   end
 
   describe '#period' do
+    let(:bill_period) { decorator.period }
+
     it 'returns when the start date' do
-      pending
+      expect(bill_period.from).to eql('2015-09-05')
     end
 
     it 'returns when the end date' do
-      pending
+      expect(bill_period.to).to eql('2015-10-05')
     end
   end
 end
