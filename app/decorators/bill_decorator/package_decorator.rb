f5de18ae72dddf00b04f1a04c34e3925f19ab53d
Joe Guest <joe.a.guest@gmail.com>
2015-10-12 08:17:14 +0100

    PackageDecorator updated, with spec

--- app/decorators/bill_decorator/package_decorator.rb
+++ app/decorators/bill_decorator/package_decorator.rb
@@ -1,7 +1,51 @@
 class PackageDecorator
-  def packages
+  def initialize(package)
+    @package = package
+  end
+
+  def self.decorate(package)
+    new(package)
+  end
+
+  def subscriptions
+    subscription_list.map do |subscription|
+      SubscriptionDecorator.decorate(subscription)
+    end
   end
 
   def total
+    package['total']
+  end
+
+  private
+  attr_reader :package
+
+  def subscription_list
+    package['subscriptions']
+  end
+
+  class SubscriptionDecorator
+    def initialize(subscription)
+      @subscription = subscription
+    end
+
+    def self.decorate(subscription)
+      new(subscription)
+    end
+
+    def type
+      subscription['type']
+    end
+
+    def name
+      subscription['name']
+    end
+
+    def cost
+      subscription['cost']
+    end
+
+    private
+    attr_reader :subscription
   end
 end
--- spec/decorators/bill_decorator/package_decorator_spec.rb
+++ spec/decorators/bill_decorator/package_decorator_spec.rb
@@ -1,29 +1,44 @@
 require './app/decorators/bill_decorator/package_decorator'
 
 describe PackageDecorator do
+  let(:decorator) { PackageDecorator.decorate(package) }
+  let(:package) {
+    {
+      'subscriptions' => [subscription],
+      'total' => '100.00'
+    }
+  }
+  let(:subscription) {
+    {
+      'type' => 'tv',
+      'name' => 'tv show',
+      'cost' => '1.23'
+    }
+  }
+
   describe '#subscriptions' do
-    it 'returns all packages subscribed to' do
-      pending
-    end
+    let(:decorated_subscriptions) { decorator.subscriptions }
 
     context 'each package' do
+      let(:decorated_subscription) { decorated_subscriptions.first }
+
       it 'returns its type' do
-        pending
+        expect(decorated_subscription.type).to eql('tv')
       end
 
       it 'returns its name' do
-        pending
+        expect(decorated_subscription.name).to eql('tv show')
       end
 
       it 'returns its cost' do
-        pending
+        expect(decorated_subscription.cost).to eql('1.23')
       end
     end
   end
 
   describe '#total' do
     it 'returns the total of all subscriptions' do
-      pending
+      expect(decorator.total).to eql('100.00')
     end
   end
 end
